a276842cdceab98b28c5fdbaae6226661d2b5c12
Lucky <Lucky.V@gmail.com>
2011-02-20 14:26:46 +0100

    documentation update according to the change of the code

--- IndepmodClassNotation/test/unit/src/cz/cvut/indepmod/classmodel/workspace/ClassModelGraphTest.java
+++ IndepmodClassNotation/test/unit/src/cz/cvut/indepmod/classmodel/workspace/ClassModelGraphTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package cz.cvut.indepmod.classmodel.workspace;
 
 import cz.cvut.indepmod.classmodel.Common;

3eac037bc829af11f767216004d797472f763e8c
Lucky <Lucky.V@gmail.com>
2011-02-27 12:02:28 +0100

    When user closes the unsaved workspace, a dialog with save/dont save question.

--- IndepmodClassNotation/src/cz/cvut/indepmod/classmodel/workspace/ClassModelWorkspace.java
+++ IndepmodClassNotation/src/cz/cvut/indepmod/classmodel/workspace/ClassModelWorkspace.java
@@ -7,8 +7,10 @@ import cz.cvut.indepmod.classmodel.file.ClassModelSaveCookie;
 import cz.cvut.indepmod.classmodel.file.ClassModelXMLDataObject;
 import cz.cvut.indepmod.classmodel.modelFactory.diagramModel.ClassModelDiagramDataModel;
 import cz.cvut.indepmod.classmodel.persistence.xml.ClassModelXMLCoder;
+import cz.cvut.indepmod.classmodel.resources.Resources;
 import java.awt.GridLayout;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,6 +19,9 @@ import java.util.logging.Logger;
 import javax.swing.JScrollPane;
 import org.jgraph.event.GraphModelEvent;
 import org.jgraph.event.GraphModelListener;
+import org.openide.DialogDescriptor;
+import org.openide.DialogDisplayer;
+import org.openide.NotifyDescriptor;
 import org.openide.util.lookup.AbstractLookup;
 import org.openide.util.lookup.InstanceContent;
 import org.openide.windows.CloneableTopComponent;
@@ -29,9 +34,7 @@ import org.openide.windows.TopComponent;
 public class ClassModelWorkspace extends CloneableTopComponent implements GraphModelListener {
 
     private static final Logger LOG = Logger.getLogger(ClassModelWorkspace.class.getName());
-
     private ClassModelDiagramDataModel diagramDataModel;
-    
     private ClassModelGraph graph;
     private ClassModelModel classModelAPI;
     private Map<Class<? extends ClassModelAbstractAction>, ClassModelAbstractAction> actions;
@@ -77,15 +80,24 @@ public class ClassModelWorkspace extends CloneableTopComponent implements GraphM
         this.setModified(true);
     }
 
+    @Override
+    public boolean canClose() {
+        if (!this.modified) {
+            return true;
+        } else {
+            return this.saveDialog();
+        }
+    }
+
     public void setModified(boolean modified) {
         if (!this.modified && modified) {
             this.modified = modified;
             this.lookupContent.add(this.saveCookie);
-            this.setHtmlDisplayName("<html><b>"+ this.getName() +"</b></html>");
+            this.setHtmlDisplayName("<html><b>" + this.getName() + "</b></html>");
         } else if (this.modified && !modified) {
             this.modified = modified;
             this.lookupContent.remove(this.saveCookie);
-            this.setHtmlDisplayName("<html>"+ this.getName() +"</html>");
+            this.setHtmlDisplayName("<html>" + this.getName() + "</html>");
         }
     }
 
@@ -128,7 +140,6 @@ public class ClassModelWorkspace extends CloneableTopComponent implements GraphM
 //                ClassModelRedoAction.ACTION_NAME,
 //                new ClassModelRedoAction());
 //    }
-
 //    private void initPopupMenu() {
 //        ClassModelAbstractAction deleteAction = this.actions.get(ClassModelDeleteAction.ACTION_NAME);
 //        deleteAction.setEnabled(true);
@@ -138,10 +149,40 @@ public class ClassModelWorkspace extends CloneableTopComponent implements GraphM
 //        this.popupMenu.add(deleteAction);
 //        this.popupMenu.add(editAction);
 //    }
-
     private void initLookup() {
         this.associateLookup(new AbstractLookup(this.lookupContent));
         this.lookupContent.add(this.selectedTool);
         this.lookupContent.add(this.classModelAPI);
+    }
+
+    /**
+     * Opens dialog which asks the user if he want to save the diagram or no.
+     * @return If user selects yes or no (true) or if he closed the dialog
+     * (false)
+     */
+    private boolean saveDialog() {
+        String saveStr = Resources.getString("dialog_close_workspace_save");
+        String nosaveStr = Resources.getString("dialog_close_workspace_dont_save");
+        NotifyDescriptor d = new NotifyDescriptor.Confirmation(
+                Resources.getString("dialog_close_workspace_message"),
+                Resources.getString("dialog_close_workspace_title"),
+                NotifyDescriptor.YES_NO_OPTION,
+                NotifyDescriptor.QUESTION_MESSAGE);
+        d.setOptions(new Object[]{saveStr, nosaveStr});
+        d.setValue(saveStr);
+        Object res = DialogDisplayer.getDefault().notify(d);
+
+        if (null != res && saveStr == res) {
+            try {
+                this.saveCookie.save();
+            } catch (IOException ex) {
+                LOG.log(Level.SEVERE, "Exception when saving: {0}", ex.getMessage());
+            }
+            return true;
+        } else if (res != null && nosaveStr == res) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
